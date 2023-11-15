f50d8545957f7b867c2a4d23fa690c11e75c8ab3
Javier André Hernández Hernández <andrehdz@MacBook-Pro-de-Javier-2.local>
2023-11-15 16:02:12 -0600

    fix analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,19 @@ import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 import Swiper from 'swiper';
 import SwiperCore, { Pagination } from 'swiper/core';
-
+import partytown from "@astrojs/partytown";
 SwiperCore.use([Pagination]);
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({applyBaseStyles: false}), preact()],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), preact(), partytown({
+    config: {
+      forward: ['dataLayer.push'],
+    },
+  })],
   base: '/',
   trailingSlash: 'ignore'
-
 });
